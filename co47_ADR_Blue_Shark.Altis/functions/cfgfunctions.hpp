dc2f4176935d1fc848653953933fbfe1c292a581
Noart <efisha55@gmail.com>
2015-12-07 16:53:24 +0500

    Обновление ADR Blue Shark

---
+++ co47_ADR_Blue_Shark.Altis/functions/cfgfunctions.hpp
@@ -0,0 +1,52 @@
+class QS
+{
+	tag = "QS";
+	class functions
+	{
+		file = "functions";
+		class actionClearInventory {};
+		class addAction {};
+		class addActionGetData {};
+		class addActionGetIntel {};
+		class addActionRescue {};
+		class addActionSabotage {};
+		class addActionSurrender {};
+		class AOdelete {};
+		class AOenemy {};
+		class AOminefield {};
+		class conditionClearInventory {};
+		class conditionUH80TurretActionLock {};
+		class conditionUH80TurretActionUnlock {};
+		class deleteOldSMUnits {};
+		class deleteUnits {};
+		class enemyCAS {};
+		class FindPosBuilding {};
+		class garrisonFortEAST {};
+		class garrisonFortFIA {};
+		class garrisonFortIND {};
+		class loadInventory {};
+		class PTenemyEAST {};
+		class removeAction {};
+		class removeAction0 {};
+		class removeAction1 {};
+		class respawnPilot {};
+		class rewardPlusHintJet {};
+		class saveInventory {};
+		class serverMapTP {};
+		class setSkill1 {};
+		class setSkill2 {};
+		class setSkill3 {};
+		class setSkill4 {};
+		class SMdelete {};
+		class SMenemyEAST {};
+		class SMenemyEASTintel {};
+		class SMenemyFIA {};
+		class SMenemyIND {};
+		class SMhintFAIL {};
+		class SMhintSUCCESS {};
+		class uh80Turret {};
+		class uh80TurretActions {};
+		class vMonitor {};
+		class vSetup02 {};
+	};
+};
